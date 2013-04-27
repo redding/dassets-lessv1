8fdb91ed524ec8774a3564aadaae27e851518d76
Kelly Redding <kelly@kellyredding.com>
2013-04-27 14:18:48 -0500

    basic engine unit tests
    Test the the input ext gets updated to 'css' and that the input
    content gets compiled.
    
    Pretty basic stuff.

--- lib/dassets-lessv1.rb
+++ lib/dassets-lessv1.rb
@@ -1,9 +1,18 @@
+require 'less'
 require 'dassets/engine'
 require "dassets-lessv1/version"
 
 module Dassets::Lessv1
 
   class Engine < Dassets::Engine
+
+    def ext(input_ext)
+      'css'
+    end
+
+    def compile(input_content)
+      ::Less::Engine.new(input_content).to_css
+    end
 
   end
 
--- test/unit/engine_tests.rb
+++ test/unit/engine_tests.rb
@@ -17,12 +17,27 @@ class Dassets::Lessv1::Engine
       assert_respond_to 'compile', subject
     end
 
-    should "transform the input extension to `css`" do
-      skip 'todo'
+    should "transform any input extension to `css`" do
+      assert_equal 'css', subject.ext('less')
+      assert_equal 'css', subject.ext('lss')
+      assert_equal 'css', subject.ext('lessv1')
+      assert_equal 'css', subject.ext('whatever')
     end
 
     should "compile any input content as LESS css" do
-      skip 'todo'
+      raw_css_content = read_root_file('css.css')
+      exp_css_content = read_root_file('css_compiled.css')
+      assert_equal exp_css_content, subject.compile(raw_css_content)
+
+      raw_less_content = read_root_file('normal.less')
+      exp_css_content  = read_root_file('normal_compiled.css')
+      assert_equal exp_css_content, subject.compile(raw_less_content)
+    end
+
+    private
+
+    def read_root_file(rel_path)
+      File.read(Dassets.config.root_path.join(rel_path))
     end
 
   end
